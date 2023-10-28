2920d6b66c079dd40f147641c49e568c11d61f9f
Lara Alexander <lara.alexander@hotmail.com>
2023-10-28 16:34:15 -0400

    Vercel troubleshooting

--- learnplanit/settings.py
+++ learnplanit/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', STATIC_URL)
+STATIC_URL = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 # Default primary key field type
